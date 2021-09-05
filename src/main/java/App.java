73049f5dd714cc5021f825aa618fa4121eb8ec43
SergeyBelolipetskiy1234 <89917779+SergeyBelolipetskiy1234@users.noreply.github.com>
2021-09-05 13:18:53 +0300

    First commit

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import POJO.Cat;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        boolean a = bean.equals(bean2);
+        boolean b = bean3.equals(bean4);
+
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+}
---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,24 @@
+import POJO.Cat;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+    @Scope("singleton")
+    @Bean(name= "helloworld")
+    public HelloWorld getHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World!");
+        return helloWorld;
+    }
+    @Scope("prototype")
+    @Bean(name= "cat")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setMessage("hi");
+        return cat;
+    }
+
+
+}
